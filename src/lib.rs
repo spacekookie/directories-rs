3a8eefaa22ba686fa1e9b34978adcb28c0c60e24
Katharina Fey <kookie@spacekookie.de>
2018-03-02 23:54:17 +0100

    Demonstrating a possible refactorisation of the library

--- src/lib.rs
+++ src/lib.rs
@@ -5,33 +5,31 @@ use std::path::PathBuf;
 #[cfg(target_os = "windows")] mod win;
 #[cfg(target_os = "macos")]   mod mac;
 
-#[cfg(target_os = "linux")]   pub use lin::base_directories as base_directories;
-#[cfg(target_os = "windows")] pub use win::base_directories as base_directories;
-#[cfg(target_os = "macos")]   pub use mac::base_directories as base_directories;
+#[cfg(target_os = "linux")]   pub use lin::OsBackend as OsBackend;
+#[cfg(target_os = "windows")] pub use win::OsBackend as OsBackend;
+#[cfg(target_os = "macos")]   pub use mac::OsBackend as OsBackend;
 
 #[derive(Debug, Clone)]
-pub struct BaseDirectories {
-    // home directory
-    home_dir:         PathBuf,
+pub struct BaseDirectories;
 
-    // base directories
-    cache_dir:        PathBuf,
-    config_dir:       PathBuf,
-    data_dir:         PathBuf,
-    data_local_dir:   PathBuf,
-    executable_dir:   Option<PathBuf>,
-    runtime_dir:      Option<PathBuf>,
-
-    // user directories
-    audio_dir:        PathBuf,
-    desktop_dir:      PathBuf,
-    document_dir:     PathBuf,
-    download_dir:     PathBuf,
-    font_dir:         Option<PathBuf>,
-    picture_dir:      PathBuf,
-    public_dir:       PathBuf,
-    template_dir:     Option<PathBuf>,
-    video_dir:        PathBuf
+/// A private abstraction over all OS specific modules
+trait BaseDirBackend {
+    fn home_dir() -> PathBuf;
+    fn cache_dir() -> PathBuf;
+    fn config_dir() -> PathBuf;
+    fn data_roaming_dir() -> PathBuf;
+    fn data_dir() -> PathBuf;
+    fn executable_dir() -> Option<PathBuf>;
+    fn runtime_dir() -> Option<PathBuf>;
+    fn audio_dir() -> PathBuf;
+    fn desktop_dir() -> PathBuf;
+    fn document_dir() -> PathBuf;
+    fn download_dir() -> PathBuf;
+    fn font_dir() -> Option<PathBuf>;
+    fn picture_dir() -> PathBuf;
+    fn public_dir() -> PathBuf;
+    fn template_dir() -> Option<PathBuf>;
+    fn video_dir() -> PathBuf;
 }
 
 #[derive(Debug, Clone)]
@@ -55,9 +53,10 @@ impl BaseDirectories {
     /// | Linux   | `$HOME`              | /home/eve/    |
     /// | macOS   | `$HOME`              | /Users/eve/   |
     /// | Windows | `{FOLDERID_Profile}` | C:\Users\Eve\ |
-    pub fn home_dir(&self) -> &Path {
-        self.home_dir.as_path()
+    pub fn home_dir() -> Path {
+        OsBackend::home_dir.as_path()
     }
+
     /// Returns the path to the user's cache directory.
     /// 
     /// |Platform | Value                             | Example                           |
@@ -65,9 +64,10 @@ impl BaseDirectories {
     /// | Linux   | `$XDG_CACHE_HOME` or `~/.cache/`  | /home/eve/.cache/                 |
     /// | macOS   | `$HOME/Library/Caches/`           | /Users/eve/Library/Caches/        |
     /// | Windows | `{FOLDERID_LocalAppData}\cache\`  | C:\Users\Eve\AppData\Local\cache\ |
-    pub fn cache_dir(&self) -> &Path {
-        self.cache_dir.as_path()
+    pub fn cache_dir() -> Path {
+        OsBackend::cache_dir.as_path()
     }
+
     /// Returns the path to the user's config directory.
     /// 
     /// |Platform | Value                              | Example                         |
@@ -75,9 +75,10 @@ impl BaseDirectories {
     /// | Linux   | `$XDG_CONFIG_HOME` or `~/.config/` | /home/eve/.config               |
     /// | macOS   | `$HOME/Library/Preferences/`       | /Users/eve/Library/Preferences/ |
     /// | Windows | `{FOLDERID_RoamingAppData}`        | C:\Users\Eve\AppData\Roaming\   |
-    pub fn config_dir(&self) -> &Path {
-        self.config_dir.as_path()
+    pub fn config_dir() -> Path {
+        OsBackend::config_dir.as_path()
     }
+
     /// Returns the path to the user's data directory.
     /// 
     /// |Platform | Value                                 | Example                                 |
@@ -85,9 +86,10 @@ impl BaseDirectories {
     /// | Linux   | `$XDG_DATA_HOME` or `~/.local/share/` | /home/eve/.local/share/                 |
     /// | macOS   | `$HOME/Library/Application Support/`  | /Users/eve/Library/Application Support/ |
     /// | Windows | `{FOLDERID_RoamingAppData}`           | C:\Users\Eve\AppData\Roaming\           |
-    pub fn data_roaming_dir(&self) -> &Path {
-        self.data_dir.as_path()
+    pub fn data_roaming_dir() -> Path {
+        OsBackend::data_dir.as_path()
     }
+
     /// Returns the path to the user's local data directory.
     /// 
     /// |Platform | Value                                 | Example                                 |
@@ -95,9 +97,10 @@ impl BaseDirectories {
     /// | Linux   | `$XDG_DATA_HOME` or `~/.local/share/` | /home/eve/.local/share/                 |
     /// | macOS   | `$HOME/Library/Application Support/`  | /Users/eve/Library/Application Support/ |
     /// | Windows | `{FOLDERID_LocalAppData}`             | C:\Users\Eve\AppData\Local\             |
-    pub fn data_dir(&self) -> &Path {
-        self.data_local_dir.as_path()
+    pub fn data_dir() -> Path {
+        OsBackend::data_local_dir.as_path()
     }
+
     /// Returns the path to the user's executable directory.
     /// 
     /// |Platform | Value                                                          | Example                  |
@@ -105,9 +108,10 @@ impl BaseDirectories {
     /// | Linux   | `$XDG_BIN_HOME/` or `$XDG_DATA_HOME/../bin/` or `~/.local/bin` | /home/eve/.local/bin/    |
     /// | macOS   | –                                                              | –                        |
     /// | Windows | –                                                              | –                        |
-    pub fn executable_dir(&self) -> Option<&Path> {
-        self.executable_dir.as_ref().map(|p| p.as_path())
+    pub fn executable_dir() -> Option<Path> {
+        OsBackend::executable_dir.as_ref().map(|p| p.as_path())
     }
+
     /// Returns the path to the user's runtime directory.
     /// 
     /// |Platform | Value              | Example         |
@@ -115,8 +119,8 @@ impl BaseDirectories {
     /// | Linux   | `$XDG_RUNTIME_DIR` | /run/user/1001/ |
     /// | macOS   | –                  | –               |
     /// | Windows | –                  | –               |
-    pub fn runtime_dir(&self) -> Option<&Path> {
-        self.runtime_dir.as_ref().map(|p| p.as_path())
+    pub fn runtime_dir() -> Option<Path> {
+        OsBackend::runtime_dir.as_ref().map(|p| p.as_path())
     }
 
     /// Returns the path to the user's audio directory.
@@ -126,9 +130,10 @@ impl BaseDirectories {
     /// | Linux   | `XDG_MUSIC_DIR`    | /home/eve/Music/    |
     /// | macOS   | `$HOME/Music/`     | /Users/eve/Music/   |
     /// | Windows | `{FOLDERID_Music}` | C:\Users\Eve\Music\ |
-    pub fn audio_dir(&self) -> &Path {
-        self.audio_dir.as_path()
+    pub fn audio_dir() -> Path {
+        OsBackend::audio_dir.as_path()
     }
+
     /// Returns the path to the user's desktop directory.
     /// 
     /// |Platform | Value              | Example                 |
@@ -136,9 +141,10 @@ impl BaseDirectories {
     /// | Linux   | `XDG_DESKTOP_DIR`    | /home/eve/Desktop/    |
     /// | macOS   | `$HOME/Desktop/`     | /Users/eve/Desktop/   |
     /// | Windows | `{FOLDERID_Desktop}` | C:\Users\Eve\Desktop\ |
-    pub fn desktop_dir(&self) -> &Path {
-        self.desktop_dir.as_path()
+    pub fn desktop_dir() -> Path {
+        OsBackend::desktop_dir.as_path()
     }
+
     /// Returns the path to the user's document directory.
     /// 
     /// |Platform | Value                  | Example                 |
@@ -146,9 +152,10 @@ impl BaseDirectories {
     /// | Linux   | `XDG_DOCUMENTS_DIR`    | /home/eve/Documents/    |
     /// | macOS   | `$HOME/Documents/`     | /Users/eve/Documents/   |
     /// | Windows | `{FOLDERID_Documents}` | C:\Users\Eve\Documents\ |
-    pub fn document_dir(&self) -> &Path {
-        self.document_dir.as_path()
+    pub fn document_dir() -> Path {
+        OsBackend::document_dir.as_path()
     }
+
     /// Returns the path to the user's download directory.
     /// 
     /// |Platform | Value                  | Example                 |
@@ -156,9 +163,10 @@ impl BaseDirectories {
     /// | Linux   | `XDG_DOWNLOAD_DIR`     | /home/eve/Downloads/    |
     /// | macOS   | `$HOME/Downloads/`     | /Users/eve/Downloads/   |
     /// | Windows | `{FOLDERID_Downloads}` | C:\Users\Eve\Downloads\ |
-    pub fn download_dir(&self) -> &Path {
-        self.download_dir.as_path()
+    pub fn download_dir() -> Path {
+        OsBackend::download_dir.as_path()
     }
+
     /// Returns the path to the user's font directory.
     /// 
     /// |Platform | Value                                                  | Example                       |
@@ -166,9 +174,10 @@ impl BaseDirectories {
     /// | Linux   | `$XDG_DATA_HOME/fonts/` or `$HOME/.local/share/fonts/` | /home/eve/.local/share/fonts/ |
     /// | macOS   | `$HOME/Library/Fonts/`                                 | /Users/eve/Library/Fonts/     |
     /// | Windows | –                                                      | –                             |
-    pub fn font_dir(&self) -> Option<&Path> {
-        self.font_dir.as_ref().map(|p| p.as_path())
+    pub fn font_dir() -> Option<Path> {
+        OsBackend::font_dir.as_ref().map(|p| p.as_path())
     }
+    
     /// Returns the path to the user's picture directory.
     /// 
     /// |Platform | Value                 | Example                |
@@ -176,9 +185,10 @@ impl BaseDirectories {
     /// | Linux   | `XDG_PICTURES_DIR`    | /home/eve/Pictures/    |
     /// | macOS   | `$HOME/Pictures/`     | /Users/eve/Pictures/   |
     /// | Windows | `{FOLDERID_Pictures}` | C:\Users\Eve\Pictures\ |
-    pub fn picture_dir(&self) -> &Path {
-        self.picture_dir.as_path()
+    pub fn picture_dir() -> Path {
+        OsBackend::picture_dir.as_path()
     }
+    
     /// Returns the path to the user's public directory.
     /// 
     /// |Platform | Value                 | Example            |
@@ -186,9 +196,10 @@ impl BaseDirectories {
     /// | Linux   | `XDG_PUBLICSHARE_DIR` | /home/eve/Public/  |
     /// | macOS   | `$HOME/Public/`       | /Users/eve/Public/ |
     /// | Windows | `{FOLDERID_Public}`   | C:\Users\Public\   |
-    pub fn public_dir(&self) -> &Path {
-        self.public_dir.as_path()
+    pub fn public_dir() -> Path {
+        OsBackend::public_dir.as_path()
     }
+    
     /// Returns the path to the user's template directory.
     /// 
     /// |Platform | Value                  | Example                                                   |
@@ -196,9 +207,10 @@ impl BaseDirectories {
     /// | Linux   | `XDG_TEMPLATES_DIR`    | /home/eve/Templates/                                      |
     /// | macOS   | –                      | –                                                         |
     /// | Windows | `{FOLDERID_Templates}` | C:\Users\Eve\AppData\Roaming\Microsoft\Windows\Templates\ |
-    pub fn template_dir(&self) -> Option<&Path> {
-        self.template_dir.as_ref().map(|p| p.as_path())
+    pub fn template_dir() -> Option<Path> {
+        OsBackend::template_dir
     }
+    
     /// Returns the path to the user's video directory.
     /// 
     /// |Platform | Value               | Example              |
@@ -206,8 +218,8 @@ impl BaseDirectories {
     /// | Linux   | `XDG_VIDEOS_DIR`    | /home/eve/Videos/    |
     /// | macOS   | `$HOME/Movies/`     | /Users/eve/Movies/   |
     /// | Windows | `{FOLDERID_Videos}` | C:\Users\Eve\Videos\ |
-    pub fn video_dir(&self) -> &Path {
-        self.video_dir.as_path()
+    pub fn video_dir() -> Path {
+        OsBackend::video_dir.as_path()
     }
 }
 
--- src/lin.rs
+++ src/lin.rs
@@ -2,60 +2,132 @@ use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
-use BaseDirectories;
+use BaseDirBackend;
 use ProjectDirectories;
 use strip_qualification;
 
-pub fn base_directories() -> BaseDirectories {
-    let home_dir       = env::home_dir().unwrap();
-    let cache_dir      = env::var("XDG_CACHE_HOME").ok().and_then(is_absolute_path).unwrap_or(home_dir.join(".cache"));
-    let config_dir     = env::var("XDG_CONFIG_HOME").ok().and_then(is_absolute_path).unwrap_or(home_dir.join(".config"));
-    let data_dir       = env::var("XDG_DATA_HOME").ok().and_then(is_absolute_path).unwrap_or(home_dir.join(".local/share"));
-    let data_local_dir = data_dir.clone();
-    let runtime_dir    = env::var("XDG_RUNTIME_DIR").ok().and_then(is_absolute_path);
-    let executable_dir = 
-        env::var("XDG_BIN_HOME").ok().and_then(is_absolute_path).unwrap_or({
-            let mut new_dir = data_dir.clone(); new_dir.pop(); new_dir.push("bin"); new_dir });
-    let font_dir       = data_dir.join("fonts");
+pub struct OsBackend;
+impl BaseDirBackend for OsBackend {
+    fn home_dir() -> PathBuf {
+        env::home_dir().unwrap()
+    }
 
-    BaseDirectories {
-        home_dir:       home_dir,
-        cache_dir:      cache_dir,
-        config_dir:     config_dir,
-        data_dir:       data_dir,
-        data_local_dir: data_local_dir,
-        executable_dir: Some(executable_dir),
-        runtime_dir:    runtime_dir,
+    fn cache_dir() -> PathBuf {
+        env::var("XDG_CACHE_HOME")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap_or(OsBackend::home_dir().join(".cache"))
+    }
 
-        audio_dir:      run_xdg_user_dir_command("MUSIC"),
-        desktop_dir:    run_xdg_user_dir_command("DESKTOP"),
-        document_dir:   run_xdg_user_dir_command("DOCUMENTS"),
-        download_dir:   run_xdg_user_dir_command("DOWNLOAD"),
-        font_dir:       Some(font_dir),
-        picture_dir:    run_xdg_user_dir_command("PICTURES"),
-        public_dir:     run_xdg_user_dir_command("PUBLICSHARE"),
-        template_dir:   Some(run_xdg_user_dir_command("TEMPLATES")),
-        video_dir:      run_xdg_user_dir_command("VIDEOS")
+    fn config_dir() -> PathBuf {
+        env::var("XDG_CONFIG_HOME")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap_or(OsBackend::home_dir().join(".config"))
+    }
+
+    fn data_roaming_dir() -> PathBuf {
+        env::var("XDG_DATA_HOME")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap_or(OsBackend::home_dir().join(".local/share"))
+    }
+
+    fn data_dir() -> PathBuf {
+        env::var("XDG_DATA_HOME")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap_or(OsBackend::home_dir().join(".local/share"))
+    }
+
+    fn executable_dir() -> Option<PathBuf> {
+        Some(
+            env::var("XDG_BIN_HOME")
+                .ok()
+                .and_then(is_absolute_path)
+                .unwrap_or({
+                    let mut new_dir = OsBackend::data_dir().clone();
+                    new_dir.pop();
+                    new_dir.push("bin");
+                    new_dir
+                }),
+        )
+    }
+
+    fn runtime_dir() -> Option<PathBuf> {
+        env::var("XDG_RUNTIME_DIR").ok().and_then(is_absolute_path)
+    }
+
+    fn audio_dir() -> PathBuf {
+        run_xdg_user_dir_command("MUSIC")
+    }
+
+    fn desktop_dir() -> PathBuf {
+        run_xdg_user_dir_command("DESKTOP")
+    }
+
+    fn document_dir() -> PathBuf {
+        run_xdg_user_dir_command("DOCUMENTS")
+    }
+
+    fn download_dir() -> PathBuf {
+        run_xdg_user_dir_command("DOWNLOAD")
+    }
+
+    fn font_dir() -> Option<PathBuf> {
+        Some(OsBackend::data_dir().join("fonts"))
+    }
+
+    fn picture_dir() -> PathBuf {
+        run_xdg_user_dir_command("PICTURES")
+    }
+
+    fn public_dir() -> PathBuf {
+        run_xdg_user_dir_command("PUBLICSHARE")
+    }
+
+    fn template_dir() -> Option<PathBuf> {
+        Some(run_xdg_user_dir_command("TEMPLATES"))
+    }
+
+    fn video_dir() -> PathBuf {
+        run_xdg_user_dir_command("VIDEOS")
     }
 }
 
 impl ProjectDirectories {
     pub fn from_unprocessed_string(value: &str) -> ProjectDirectories {
         let project_name = String::from(value);
-        let home_dir               = env::home_dir().unwrap();
-        let project_cache_dir      = env::var("XDG_CACHE_HOME").ok().and_then(is_absolute_path).unwrap_or(home_dir.join(".cache")).join(&value);
-        let project_config_dir     = env::var("XDG_CONFIG_HOME").ok().and_then(is_absolute_path).unwrap_or(home_dir.join(".config")).join(&value);
-        let project_data_dir       = env::var("XDG_DATA_HOME").ok().and_then(is_absolute_path).unwrap_or(home_dir.join(".local/share")).join(&value);
+        let home_dir = env::home_dir().unwrap();
+        let project_cache_dir = env::var("XDG_CACHE_HOME")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap_or(home_dir.join(".cache"))
+            .join(&value);
+        let project_config_dir = env::var("XDG_CONFIG_HOME")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap_or(home_dir.join(".config"))
+            .join(&value);
+        let project_data_dir = env::var("XDG_DATA_HOME")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap_or(home_dir.join(".local/share"))
+            .join(&value);
         let project_data_local_dir = project_data_dir.clone();
-        let project_runtime_dir    = env::var("XDG_RUNTIME_DIR").ok().and_then(is_absolute_path).unwrap().join(&value);
+        let project_runtime_dir = env::var("XDG_RUNTIME_DIR")
+            .ok()
+            .and_then(is_absolute_path)
+            .unwrap()
+            .join(&value);
 
         ProjectDirectories {
-            project_name:           project_name,
-            project_cache_dir:      project_cache_dir,
-            project_config_dir:     project_config_dir,
-            project_data_dir:       project_data_dir,
+            project_name: project_name,
+            project_cache_dir: project_cache_dir,
+            project_config_dir: project_config_dir,
+            project_data_dir: project_data_dir,
             project_data_local_dir: project_data_local_dir,
-            project_runtime_dir:    Some(project_runtime_dir)
+            project_runtime_dir: Some(project_runtime_dir),
         }
     }
 
@@ -68,7 +140,6 @@ impl ProjectDirectories {
         let name = strip_qualification(qualified_project_name).to_lowercase();
         ProjectDirectories::from_unprocessed_string(name.trim())
     }
-
 }
 
 fn is_absolute_path(path: String) -> Option<PathBuf> {
@@ -81,7 +152,11 @@ fn is_absolute_path(path: String) -> Option<PathBuf> {
 }
 
 fn run_xdg_user_dir_command(arg: &str) -> PathBuf {
-    let mut out  = Command::new("xdg-user-dir").arg(arg).output().expect("failed to execute process").stdout;
+    let mut out = Command::new("xdg-user-dir")
+        .arg(arg)
+        .output()
+        .expect("failed to execute process")
+        .stdout;
     let out_len = out.len();
     out.truncate(out_len - 1);
     PathBuf::from(String::from_utf8(out).unwrap())
@@ -108,23 +183,23 @@ mod tests {
 
     #[test]
     fn test_trim_and_replace_spaces_with_hyphens_then_lowercase() {
-        let input1    = "Bar App";
-        let actual1   = trim_and_replace_spaces_with_hyphens_then_lowercase(input1);
+        let input1 = "Bar App";
+        let actual1 = trim_and_replace_spaces_with_hyphens_then_lowercase(input1);
         let expected1 = "bar-app";
         assert_eq!(expected1, actual1);
 
-        let input2    = "BarApp-Foo";
-        let actual2   = trim_and_replace_spaces_with_hyphens_then_lowercase(input2);
+        let input2 = "BarApp-Foo";
+        let actual2 = trim_and_replace_spaces_with_hyphens_then_lowercase(input2);
         let expected2 = "barapp-foo";
         assert_eq!(expected2, actual2);
 
-        let input3    = " Bar App ";
-        let actual3   = trim_and_replace_spaces_with_hyphens_then_lowercase(input3);
+        let input3 = " Bar App ";
+        let actual3 = trim_and_replace_spaces_with_hyphens_then_lowercase(input3);
         let expected3 = "bar-app";
         assert_eq!(expected3, actual3);
 
-        let input4    = "  Bar  App  ";
-        let actual4   = trim_and_replace_spaces_with_hyphens_then_lowercase(input4);
+        let input4 = "  Bar  App  ";
+        let actual4 = trim_and_replace_spaces_with_hyphens_then_lowercase(input4);
         let expected4 = "bar-app";
         assert_eq!(expected4, actual4);
     }
